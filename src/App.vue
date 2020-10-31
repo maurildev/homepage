a9b0d24e5eab3b8233708ca03d92fda09b3eaac5
maurildev <mauril.salb@googlemail.com>
2020-10-31 23:39:17 +0100

    [no-feat] add placeholder background img

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,30 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "styles/global.scss";
+
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  @include tablet {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #bcada1;
+    background-image: url(https://images.pexels.com/photos/172277/pexels-photo-172277.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover; 
+  }
 }
 </style>
