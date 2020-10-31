b890720a2461ca5b270a1afac366b5bf97287e24
maurildev <mauril.salb@googlemail.com>
2020-10-31 23:12:35 +0100

    [feat/language-view] config for multi-language

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,44 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import { deTranslations } from '../config/deTranslations';
+import { enTranslations } from '../config/enTranslations';
+
+const state = {
+  translations: undefined,
+}
+
+const getters = {
+  translations(state) {
+    return state.translations;
+  }
+}
+
+const mutations = {
+  initiliaseLanguageFromLocalStorage(state){
+    switch (localStorage.getItem('languageCode')) {
+      case 'de':
+        state.translations = deTranslations;
+        break;
+      case 'en':
+        state.translations = enTranslations;
+        break;
+    }
+  },
+  setLanguage(state, languageCode) {
+    switch (languageCode) {
+      case 'de':
+        state.translations = deTranslations;
+        localStorage.setItem('languageCode', 'de');
+        break;
+      case 'en':
+        state.translations = enTranslations;
+        localStorage.setItem('languageCode', 'en');
+        break;
+    }
+  }
+}
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state,
+  getters,
+  mutations
 })
