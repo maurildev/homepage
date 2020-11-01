4f462fb1c2366dd2ad104bdf0e00e140aec2eecc
maurildev <mauril.salb@googlemail.com>
2020-11-01 19:00:46 +0100

    [feat/chat-view] add chat-view

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,29 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import Chat from '@/views/Chat.vue';
 import Language from '@/views/Language.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const routes = [
   {
     path: '/',
     name: 'Language',
     component: Language
-  }
+  },
+  {
+    path: '/de/chat/',
+    name: 'deChat',
+    meta: {
+      languageCode: "de",
+    },
+    component: Chat
+  },
+  {
+    path: '/en/chat/',
+    name: 'enChat',
+    meta: {
+      languageCode: "en",
+    },
+    component: Chat
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/Chat.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="chat-screen">
+      <v-nav-bar :isBackButtonVisible="false" :isHomeButtonHighlighted="highlightHomeButton" />
+      <div class="chat-screen__chat-simulator">
+        <v-chat-simulator  :messages="messages" @completed="highlightHomeButton = true"/>
+      </div>
+    </div>
+</template>
+
+<script>
+import ChatSimulator from 'vue-chat-simulator';
+import NavBar from '@/components/shared/navigation/NavBar.vue';
+
+export default {
+  components: {
+    'v-chat-simulator': ChatSimulator,
+    'v-nav-bar': NavBar
+  },
+  data() {
+    return {
+      highlightHomeButton: false,
+      messages: [
+        'Heey 👋🏽 ',
+        'Ich bin Mauril, ein Frontend Developer',
+        'Ich habe mir überlegt, wie du mich am besten kennenlernst',
+        'Und es gibt doch nichts persönlicheres als das eigene Smartphone oder ?!',
+        'Also viel Spaß, beim stöbern ',
+      ],
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "styles/global.scss";
+
+.chat-screen {
+  @include screen;
+  background: $white;
+}
+</style>
+
