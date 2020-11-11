7dd49695932724a482f21cc4fb4cdb9b0c66428e
maurildev <mauril.salb@googlemail.com>
2020-11-11 22:07:07 +0100

    [feat/about-me-chat-view] create about chat view

---
+++ src/views/ChatAboutMe.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="chat-about-screen">
+    <v-nav-bar />
+    <div class="chat-about-screen__chat-simulator">
+      <v-chat-simulator
+        :messages="messages"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import ChatSimulator from "vue-chat-simulator";
+import NavBar from "@/components/shared/navigation/NavBar.vue";
+import { mapGetters } from "vuex";
+
+export default {
+  components: {
+    "v-chat-simulator": ChatSimulator,
+    "v-nav-bar": NavBar,
+  },
+  computed: {
+    ...mapGetters(["translations"]),
+    messages() {
+      return [
+        this.translations.aboutMeChat.chatMessage1,
+        this.translations.aboutMeChat.chatMessage2,
+        this.translations.aboutMeChat.chatMessage3,
+        this.translations.aboutMeChat.chatMessage4,
+        this.translations.aboutMeChat.chatMessage5,
+      ];
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "styles/global.scss";
+
+.chat-about-screen {
+  @include screen;
+  background: $white;
+}
+</style>
