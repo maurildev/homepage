69477afc789d64b3e4563a8310226f5b2080a7bd
maurildev <mauril.salb@googlemail.com>
2020-11-12 20:54:21 +0100

    [feat/technology-view] create logo component

---
+++ src/components/shared/media/Logo.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="logo">
+    <img
+      :class="{
+        'logo__img--small': small,
+        'logo__img--medium': medium,
+        'logo__img--big': big,
+        'logo__img': (small === false) && (medium === false) && (big === false),
+      }"
+      :src="imgUrl"
+    />
+    <span v-if="text" class="logo__text">{{ text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Logo",
+  props: {
+    imgUrl: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: false,
+    },
+    small: {
+      type: Boolean,
+      required: false,
+    },
+    medium: {
+      type: Boolean,
+      required: false,
+    },
+    big: {
+      type: Boolean,
+      required: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  &__img--small {
+    width: 35px;
+    height: 35px;
+  }
+
+  &__img,
+  &__img--medium {
+    width: 50px;
+    height: 50px;
+  }
+
+  &__img--big {
+    width: 75px;
+    height: 75px;
+  }
+
+  &__text {
+    display: block;
+    font-size: 12px;
+    font-weight: bold;
+    margin-top: 10px;
+  }
+}
+</style>
