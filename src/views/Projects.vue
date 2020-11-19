782d27bcd936a470da1af23980ad269f865469b0
maurildev <mauril.salb@googlemail.com>
2020-11-19 21:02:42 +0100

    [feat/project-view] use projects file for data

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="projects-screen">
     <v-nav-bar />
-    <div
+    <router-link
         v-for="project in projects"
         :key="project.company" 
+        :to="{ name: `project-${$route.meta.languageCode}`, params: { project: project.company.toLowerCase().replace(' ', '-') }}"
         class="projects-screen-project-link"
     >
         <v-avatar 
-                 :imgUrl="project.img"
-                 small
+            :imgUrl="project.img"
+            small
         />
         <div class="projects-screen-project-link-description">
             <h2 class="projects-screen-project-link-description__text--bold">{{ project.company }}</h2>
@@ -16,13 +17,14 @@
                 {{ project.from }} - {{ project.to }}
             </span>
         </div>
-    </div>
+    </router-link>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/shared/navigation/NavBar.vue";
 import Avatar from "@/components/shared/media/Avatar.vue";
+import { projects } from "@/config/projects/projects";
 
 export default {
   name: ["Projects"],
@@ -31,58 +33,9 @@ export default {
     "v-avatar": Avatar,
   },
   data() {
-      return {
-          projects: [
-              {
-                img: require("@/assets/company-logos/mercedes.jpg"),
-                company: "Mercedes-Benz",
-                from: "01.10.2020",
-                to: "Heute"
-              },
-              {
-                img: require("@/assets/company-logos/boehringer-ingelheim.png"),
-                company: "Boehringer Ingelheim",
-                from: "01.07.2020",
-                to: "31.09.2020"
-              },
-              {
-                img:  require("@/assets/company-logos/blackhound.png"),
-                company: "blackHound Agency",
-                from: "01.04.2020",
-                to: "31.06.2020"
-              },
-              {
-                img: require("@/assets/company-logos/ikea.jpg"),
-                company: "IKEA Deutschland",
-                from: "01.09.2017",
-                to: "31.03.2020"
-              },
-              {
-                img: require("@/assets/company-logos/2vizcon.png"),
-                company: "2VizCon",
-                from: "01.01.2017",
-                to: "31.08.2017"
-              },
-              {
-                img: require("@/assets/company-logos/deutsche-bahn.png"),
-                company: "Deutsche Bahn",
-                from: "01.04.2016",
-                to: "31.09.2016"
-              },
-              {
-                img: require("@/assets/company-logos/deutscher-wetterdienst.jpg"),
-                company: "Deutscher Wetterdienst",
-                from: "01.03.2015",
-                to: "31.10.2016"
-              },
-              {
-                img: require("@/assets/company-logos/frauas.png"),
-                company: "Frankfurt UAS",
-                from: "01.10.2011",
-                to: "01.10.2015"
-              },
-          ]
-      }
+    return {
+      projects
+    }
   }
 };
 </script>
