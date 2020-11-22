065d7d4af65932c52ce50f9f61a4793a8333a454
maurildev <mauril.salb@googlemail.com>
2020-11-22 19:18:23 +0100

    [no-feat] serve files from homepage

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/maurildev-homepage/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/homepage/' : '/',
   configureWebpack: {
     resolve: {
       alias: {
